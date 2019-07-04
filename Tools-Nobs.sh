345df176eac19fb680fe46ccdd475bccc27926b6
CoumingX <52475322+CoumingX@users.noreply.github.com>
2019-07-04 20:46:51 +0700

    Update Tools-Nobs.sh

--- Tools-Nobs.sh
+++ Tools-Nobs.sh
@@ -5,6 +5,13 @@
 #MikirMakeOtakGblk!
 clear
 cd modul
+   /-----------------|
+  / |----------------|
+ |  |  -Tools-Nobs-  |
+ |  |   -CoumingX-   |
+  \ |----------------|
+   \-----------------|
+
 blue='\033[34;1m'
 green='\033[32;1m'  
 purple='\033[35;1m'
